97f8928c94e59838628f01a86ecb3f325ccf389d
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-03-25 18:26:55 +0000

    nix_ci_handler: handle signatures, ignore method

--- src/nix_ci_handler.erl
+++ src/nix_ci_handler.erl
@@ -2,24 +2,39 @@
 
 -export([init/2]).
 
+secret() -> os:getenv("NIX_CI_GITHUB_SECRET").
+
 init(Req0, Opts) ->
-    Method = cowboy_req:method(Req0),
     {ok, Body, Req1} = cowboy_req:read_body(Req0),
     Event = cowboy_req:header(<<"x-github-event">>, Req1),
-    Resp = echo(Method, Event, Body, Req1),
+    Signature = cowboy_req:header(<<"x-hub-signature">>, Req1),
+    Resp = handle(Event, Body, Req1, Signature),
     {ok, Resp, Opts}.
 
-echo(<<"POST">>, <<"pull_request">>, Body, Req) ->
+handle(Event, Body, Req, Signature0) ->
+    Signature1 = encode_signature(crypto:hmac(sha, secret(), Body)),
+    if Signature0 == Signature1 ->
+	    handle_trusted(Event, Body, Req);
+       true ->
+	    cowboy_req:reply(400, Req)
+    end.
+
+binary_to_hex(Bin) ->
+    lists:flatten([io_lib:format("~2.16.0b", [X]) ||
+		      X <- binary_to_list(Bin)]).
+
+encode_signature(Bin) ->
+    list_to_binary("sha1=" ++ binary_to_hex(Bin)).
+
+handle_trusted(<<"pull_request">>, Body, Req) ->
     {ok, Payload, _Rest} = json:decode(Body),
     #{<<"pull_request">> := #{<<"head">> := HEAD}} = Payload,
     #{<<"repo">> := #{<<"full_name">> := Name}} = HEAD,
     #{<<"sha">> := Rev} = HEAD,
     spawn(fun() -> build({Name, Rev}) end),
     cowboy_req:reply(202, Req);
-echo(<<"POST">>, _, _, Req) ->
-    cowboy_req:reply(200, Req);
-echo(_, _, _, Req) ->
-    cowboy_req:reply(405, Req).
+handle_trusted(_, _, Req) ->
+    cowboy_req:reply(200, Req).
 
 build(Coord) ->
     nix_ci_github:status(Coord, <<"Building...">>, <<"pending">>),
